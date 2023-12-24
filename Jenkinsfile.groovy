5234435c8a33abd37992cc94295e141e26e41f6d
sanseyvich <sanseyvich@gmail.com>
2023-12-24 11:17:17 +0200

    first commit

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,19 @@
+pipeline{
+
+    agent any
+
+    stages{
+
+        stage('Run tests'){
+            steps {
+                sh "docker-compose up"
+            }
+        }
+
+        stage('Cleanup'){
+            steps {
+                sh "docker-compose down"
+            }            
+        }  
+
+}
